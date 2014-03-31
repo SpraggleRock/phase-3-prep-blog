a7fb2950c7f0e4be1df4f0a35ecea7d9587d3690
Apprentice <apprentice@dbc-chicago-007.local>
2014-03-30 21:29:53 -0500

    another commit?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaptainsLog::Application.config.secret_key_base = 'bb9744ea6e47b8eef5eb57ba6fc3964ce9068d8b741ed7f0fc2261e5cd5b4a6b239976e98bdf73a2244d91be81d81e6b4d49c91a7ad0d74c56e73e9b394d6bdd'
